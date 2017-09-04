01093aa38966dc44d8d979c316b444f473373e19
unknown <rebecca.anastasiou@gmail.com>
2017-09-04 13:44:31 +0200

    allt i ett

--- Program.cs
+++ Program.cs
@@ -10,14 +10,17 @@ namespace kortlekssimulering
     {
         static void Main(string[] args)
         {
-            int[] cards = new int[52];
+
+            string[] cards = new string[52];
             Random r = new Random();
             int k = 0;
             bool[] choice = new bool[52];
             bool done = false;
             int c = 0;
             bool moreCard = true;
-            string[] cardDrawn = new string[2];
+            int poäng = 0;
+            int poängSumma = 0;
+
 
             for (int i = 0; i < 52; i++)
             {
@@ -25,40 +28,74 @@ namespace kortlekssimulering
             }
             for (int i = 0; i < 13; i++)
             {
-                cards[i] = i + 1;
+                cards[i] = "hjärter " + (i + 1);
             }
             for (int i = 13; i < 26; i++)
             {
-                cards[i] = i - 12;
+                cards[i] = "klöver " + (i - 12);
             }
             for (int i = 26; i < 39; i++)
             {
-                cards[i] = i - 25;
+                cards[i] = "ruter " + (i - 25);
             }
             for (int i = 39; i < 52; i++)
             {
-                cards[i] = i - 38;
+                cards[i] = "spader " + (i - 38);
             }
             while (moreCard)
-
             {
-                Console.WriteLine("Vill du dra ett kort?");
+                done = false;
+                Console.WriteLine("Vill du dra ett kort? ");
                 string pick = Console.ReadLine();
-                if (pick == "ja" || pick == "Ja!" || pick == "JA")
+                if (pick == "ja" || pick == "Ja" || pick == "JA")
+                {
+                    moreCard = true;
+                }
+                if (pick == "nej" || pick == "Nej" || pick == "NEJ")
                 {
                     moreCard = false;
                 }
 
-                k = r.Next(0, 52);
-                if (choice[k])
+
+                if (moreCard)
                 {
-                    Console.WriteLine(cards[k]);
-                    choice[k] = false;
-                    c++;
+                    while (!done)
+                    {
+                        k = r.Next(0, 52);
+                        if (choice[k])
+                        {
+                            Console.Write("Du drog kortet " + cards[k]);
+                            choice[k] = false;
+                            c++;
+                            string[] cardsDrawn = cards[k].Split(' ');
+                            poäng = Convert.ToInt32(cardsDrawn[1]);
+                            poängSumma += poäng;
+                            if (poängSumma > 21)
+                            {
+                                Console.WriteLine(", du förlorade! Du fick mer än 21");
+                                done = true;
+                                moreCard = false;
+                            }
+                            else if (poängSumma == 21)
+                            {
+                                Console.WriteLine(". Grattis du fick 21 och vann!");
+                                Console.ReadLine();
+                                return;
+                            }
+                            else
+                            {
+                                Console.WriteLine(" och din poängsumma är " + poängSumma);
+                                done = true;
+                            }
+                        }
+                    }
                 }
-                string
+                else
+                {
+                    Console.WriteLine("Din poängsumma är " + poängSumma);
+                }
             }
-            Console.WriteLine(c);
+
         }
     }
 }
