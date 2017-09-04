ede53d0fb67483722d3914c8609c5a4d6869e1ae
unknown <rebecca.anastasiou@gmail.com>
2017-09-04 13:12:11 +0200

    kort

--- Program.cs
+++ Program.cs
@@ -10,6 +10,55 @@ namespace kortlekssimulering
     {
         static void Main(string[] args)
         {
+            int[] cards = new int[52];
+            Random r = new Random();
+            int k = 0;
+            bool[] choice = new bool[52];
+            bool done = false;
+            int c = 0;
+            bool moreCard = true;
+            string[] cardDrawn = new string[2];
+
+            for (int i = 0; i < 52; i++)
+            {
+                choice[i] = true;
+            }
+            for (int i = 0; i < 13; i++)
+            {
+                cards[i] = i + 1;
+            }
+            for (int i = 13; i < 26; i++)
+            {
+                cards[i] = i - 12;
+            }
+            for (int i = 26; i < 39; i++)
+            {
+                cards[i] = i - 25;
+            }
+            for (int i = 39; i < 52; i++)
+            {
+                cards[i] = i - 38;
+            }
+            while (moreCard)
+
+            {
+                Console.WriteLine("Vill du dra ett kort?");
+                string pick = Console.ReadLine();
+                if (pick == "ja" || pick == "Ja!" || pick == "JA")
+                {
+                    moreCard = false;
+                }
+
+                k = r.Next(0, 52);
+                if (choice[k])
+                {
+                    Console.WriteLine(cards[k]);
+                    choice[k] = false;
+                    c++;
+                }
+                string
+            }
+            Console.WriteLine(c);
         }
     }
 }
